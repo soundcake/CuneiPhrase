a602804bd02085f465d57c74649aedc692dec2c2
soundcake <luke@soundcake.co.uk>
2018-03-10 13:38:12 +0000

    w.i.p.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-alert('chicken');
+$(document).ready(function () {
+    //alert('and now jquery is working');
+    //$('body').css({'font-family':''});
+    $("head").prepend("<style type=\"text/css\">" +
+        "@font-face {\n" +
+        "\tfont-family: \"Behistun\";\n" +
+        "\tsrc: local('☺'), url('/../fonts/Behistun.otf') format('truetype');\n" +
+        "}\n" +
+        "\tbody p {\n" +
+        "\tfont-family: Verdana !important;\n" +
+        "\tfont-weight: bold !important;\n" +
+        "\tfont-size: 300px !important;\n" +
+        "\tcolor: red !important;\n" +
+        "}\n" +
+        "</style>");
+});
